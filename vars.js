6bc93de790a4992b56c25a2c0f7fc574531a0a66
Jean-Luc <jean-luc.a@ideafactory.fr>
2019-11-06 08:27:42 +0100

    Initial commit

---
+++ vars.js
@@ -0,0 +1,12 @@
+/**
+ * @name vars.js
+ * @author Aélion
+ * @version 1.0.0
+ * @abstract Defining and using vars
+ */
+
+// Sets the "myName" variable with some string
+let myName = 'Aubert'
+
+// Output the content of "myName" variable
+console.log('Bonjour ' + myName);
